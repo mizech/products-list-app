c1002c983c0993073371a1d90afa396c5bb0886a
mizech <michael.zech@hotmail.de>
2024-10-21 16:37:41 +0200

    Initial Commit

---
+++ ProductsList/ProductsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsListApp.swift
+//  ProductsList
+//
+//  Created by Michael on 21.10.24.
+//
+
+import SwiftUI
+
+@main
+struct ProductsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
