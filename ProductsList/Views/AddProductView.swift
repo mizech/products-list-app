f86a8979482250ab2e45c847d7b0afec4edd9b4e
mizech <michael.zech@hotmail.de>
2024-11-23 14:29:16 +0100

    Implemented: Add-Product sheet

--- ProductsList/Views/AddProductView.swift
+++ ProductsList/Views/AddProductView.swift
@@ -3,13 +3,71 @@ import SwiftData
 
 struct AddProductView: View {
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var category = ""
+    @State private var name = ""
+    @State private var brand = ""
+    @State private var desc = ""
+    @State private var basePrice: Double? = nil
+    @State private var inStock = false
+    @State private var stock: Int? = nil
     
     var body: some View {
-        VStack {
-            Button("Add new product") {
-                let product = Product(productCategory: "Category 01", name: "Name 01", brand: "Brand 01", desc: "Description 01", basePrice: 99.99, inStock: true, stock: 4)
-                modelContext.insert(product)
+        NavigationView {
+            Form {
+                Section("Product category") {
+                    TextField("Enter a category", text: $category)
+                }
+                Section("Product name") {
+                    TextField("Enter a name", text: $name)
+                }
+                Section("Product brand") {
+                    TextField("Enter the brand", text: $brand)
+                }
+                Section("Product description") {
+                    TextField("Enter a description", text: $desc)
+                }
+                Section("Product base-price") {
+                    TextField("Enter a price", value: $basePrice, format: .number)
+                        .keyboardType(.numberPad)
+                }
+                Section("Product stock") {
+                    TextField("How many products are on stock?", value: $stock, format: .number)
+                        .keyboardType(.numberPad)
+                }
+                
+                Button {
+                    let product = Product(
+                        productCategory: category,
+                        name: name,
+                        brand: brand,
+                        desc: desc,
+                        basePrice: basePrice ?? 0.0,
+                        inStock: stock ?? 0 > 0 ? false : true,
+                        stock: stock ?? 0
+                    )
+                    modelContext.insert(product)
+                    dismiss()
+                } label: {
+                    Text("Add product")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 40)
+                        .foregroundStyle(.white)
+                        .background(.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                }
             }
+                .navigationTitle("Enter product-data")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add product", systemImage: "xmark.circle") {
+                            dismiss()
+                        }
+                    }
+                }
         }
     }
 }
